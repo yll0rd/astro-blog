4028a2c3ab596286b36f9764fa352af83b71585b
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-06-03 17:29:07 +0100

    Using constants

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "TechPeople Blog";
+export const SITE_DESCRIPTION = "Articles, stories and tutorials from TechPeople";
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
