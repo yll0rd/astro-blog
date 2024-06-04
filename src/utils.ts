0f03863b327634aabdb0c43525f34e90131fc1cb
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-06-04 13:23:04 +0100

    Tags Component

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+// Format the date to a string
 export const formatDate = (date: Date): string => {
     const options: Intl.DateTimeFormatOptions = {
         year: 'numeric',
@@ -5,4 +6,11 @@ export const formatDate = (date: Date): string => {
         day: 'numeric'
     }
     return new Date(date).toLocaleDateString(undefined, options)
+}
+
+// Capitalize the first letter
+export const capitalize = (str: string): string => {
+    if (typeof str !== 'string' || str.length === 0)
+        return str
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
 }
