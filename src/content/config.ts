e1be15eb713270c08f1b3316734e0b0f9a64e487
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-06-04 11:06:00 +0100

    Collection Scema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,15 @@
-import { defineCollection } from "astro:content"
+import { z, defineCollection } from "astro:content"
 
-const blogCollection = defineCollection({});
+const blogCollection = defineCollection({,
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date(),
+        author: z.string(),
+        image: z.string(),
+        tags: z.array(z.string()),
+    })
+});
 
 export const collections = {
     blog: blogCollection,
